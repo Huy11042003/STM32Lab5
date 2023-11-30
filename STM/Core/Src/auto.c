83401eff4aa245f7291b10e0c930aa631f48afaf
thuocsolai <huy.pham1104@hcmut.edu.vn>
2023-11-30 22:27:45 +0700

    Update

--- STM/Core/Src/auto.c
+++ STM/Core/Src/auto.c
@@ -6,13 +6,10 @@
  */
 #include "auto.h"
 
-//void led_fsm(){
-//	switch(autostate){
 		void RED_GREEN(){
 			if (isButtonPressed(0) == 1 ){
 				HAL_GPIO_WritePin(GPIOA, EN0_Pin|EN1_Pin|EN2_Pin|EN3_Pin
 										  |EN4_Pin|EN5_Pin, GPIO_PIN_SET);
-//				MODE = MODE2;
 				HAL_GPIO_WritePin(GPIOA, RED1_Pin|YELLOW1_Pin|GREEN1_Pin|RED2_Pin
 											  |YELLOW2_Pin|GREEN2_Pin, GPIO_PIN_SET);
 				SCH_Delete_Task(0);
@@ -20,27 +17,21 @@
 				SCH_Add_Task(MODE2, 1, 50);
 				SCH_Add_Task(displayIncreaseRed, 1, 50);
 				return;
-//				break;
 			}
 			displayFirstLedCouple(RED);
 			displaySecondLedCouple(GREEN);
-//			if(timer_flag[0] == 1){
 				countdownFirstCoupleLed--;
 				countdownSecondCoupleLed--;
 				if(countdownSecondCoupleLed == 0){
 					countdownSecondCoupleLed = yellowDelay;
-//					autostate = RED_YELLOW;
 					SCH_Delete_Task(1);
 					SCH_Add_Task(RED_YELLOW, 1, 100);
 				}
-//				setTimer(0, 100);
 			}
-//			break;
 		void RED_YELLOW(){
 			if (isButtonPressed(0) == 1 ){
 				HAL_GPIO_WritePin(GPIOA, EN0_Pin|EN1_Pin|EN2_Pin|EN3_Pin
 										  |EN4_Pin|EN5_Pin, GPIO_PIN_SET);
-//				MODE = MODE2;
 				HAL_GPIO_WritePin(GPIOA, RED1_Pin|YELLOW1_Pin|GREEN1_Pin|RED2_Pin
 											  |YELLOW2_Pin|GREEN2_Pin, GPIO_PIN_SET);
 				SCH_Delete_Task(0);
@@ -48,28 +39,22 @@
 				SCH_Add_Task(MODE2, 1, 50);
 				SCH_Add_Task(displayIncreaseRed, 1, 50);
 				return;
-//				break;
 			}
 			displayFirstLedCouple(RED);
 			displaySecondLedCouple(YELLOW);
-//			if(timer_flag[0] == 1){
 				countdownFirstCoupleLed--;
 				countdownSecondCoupleLed--;
 				if(countdownSecondCoupleLed == 0){
 					countdownSecondCoupleLed = redDelay;
 					countdownFirstCoupleLed = greenDelay;
-//					autostate = GREEN_RED;
 					SCH_Delete_Task(1);
 					SCH_Add_Task(GREEN_RED, 1, 100);
 				}
-//				setTimer(0, 100);
 			}
-//			break;
 		void GREEN_RED(){
 			if (isButtonPressed(0) == 1 ){
 				HAL_GPIO_WritePin(GPIOA, EN0_Pin|EN1_Pin|EN2_Pin|EN3_Pin
 										  |EN4_Pin|EN5_Pin, GPIO_PIN_SET);
-//				MODE = MODE2;
 				HAL_GPIO_WritePin(GPIOA, RED1_Pin|YELLOW1_Pin|GREEN1_Pin|RED2_Pin
 											  |YELLOW2_Pin|GREEN2_Pin, GPIO_PIN_SET);
 				SCH_Delete_Task(0);
@@ -77,27 +62,21 @@
 				SCH_Add_Task(MODE2, 1, 50);
 				SCH_Add_Task(displayIncreaseRed, 1, 50);
 				return;
-//				break;
 			}
 			displayFirstLedCouple(GREEN);
 			displaySecondLedCouple(RED);
-//			if(timer_flag[0] == 1){
 				countdownFirstCoupleLed--;
 				countdownSecondCoupleLed--;
 				if(countdownFirstCoupleLed == 0){
 					countdownFirstCoupleLed = yellowDelay;
-//					autostate = YELLOW_RED;
 					SCH_Delete_Task(1);
 					SCH_Add_Task(YELLOW_RED, 1, 100);
 				}
-//				setTimer(0, 100);
 			}
-//			break;
 		void YELLOW_RED(){
 			if (isButtonPressed(0) == 1 ){
 				HAL_GPIO_WritePin(GPIOA, EN0_Pin|EN1_Pin|EN2_Pin|EN3_Pin
 										  |EN4_Pin|EN5_Pin, GPIO_PIN_SET);
-//				MODE = MODE2;
 				HAL_GPIO_WritePin(GPIOA, RED1_Pin|YELLOW1_Pin|GREEN1_Pin|RED2_Pin
 											  |YELLOW2_Pin|GREEN2_Pin, GPIO_PIN_SET);
 				SCH_Delete_Task(0);
@@ -105,25 +84,18 @@
 				SCH_Add_Task(MODE2, 1, 50);
 				SCH_Add_Task(displayIncreaseRed, 1, 50);
 				return;
-//				break;
 			}
 			displayFirstLedCouple(YELLOW);
 			displaySecondLedCouple(RED);
-//			if(timer_flag[0] == 1){
 				countdownFirstCoupleLed--;
 				countdownSecondCoupleLed--;
 				if(countdownFirstCoupleLed == 0){
 					countdownSecondCoupleLed = greenDelay;
 					countdownFirstCoupleLed = redDelay;
-//					autostate = RED_GREEN;
 					SCH_Delete_Task(1);
 					SCH_Add_Task(RED_GREEN, 1, 100);
 				}
-//				setTimer(0, 100);
 			}
-//			break;
-//	}
-//}
 void seg_fsm(){
 	switch(seg7AutoState){
 		case FIRST:
@@ -138,9 +110,3 @@ void seg_fsm(){
 			break;
 	}
 }
-//void auto_run(){
-//	if(MODE == MODE1){
-//		led_fsm();
-//		seg_fsm();
-//	}
-//}
